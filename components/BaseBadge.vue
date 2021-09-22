b77ca8041b07ffd45ed06a2ddb79a150d1a875f4
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2021-09-22 18:12:58 +0200

    BaseBadge Component updated

--- components/BaseBadge.vue
+++ components/BaseBadge.vue
@@ -10,18 +10,10 @@ export default {
   computed: {
     classBadge() {
       let color
-      if (this.metacritic >= 75) color = 'green'
-      else if (this.metacritic >= 50) color = 'yellow'
-      else color = 'red'
-      return [
-        'absolute',
-        'rounded',
-        'top-2',
-        'left-2',
-        'px-2',
-        'py-1',
-        `bg-${color}-600`,
-      ]
+      if (this.metacritic >= 75) color = 'bg-green-600'
+      else if (this.metacritic >= 50) color = 'bg-yellow-600'
+      else color = 'bg-red-600'
+      return ['absolute', 'rounded', 'top-2', 'left-2', 'px-2', 'py-1', color]
     },
   },
 }
